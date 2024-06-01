ec0f017f6081cf09decfde3c71514406e1779264
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-05-31 21:05:32 -0600

    diseño de auth-login

--- Admin/src/Pages/Authentication/Login.js
+++ Admin/src/Pages/Authentication/Login.js
@@ -117,172 +117,173 @@ const Login = (props) => {
   });
 
   return (
-    <React.Fragment >
-        <div className="context">
-        
-    </div>
+    <React.Fragment>
+      <div className="context"></div>
 
+      <div className="area">
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
 
-<div className="area" >
-            <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            
-      <div className="account-pages my-2 pt-3">
-      <Container>
-        <Row className="justify-content-center">
-          <Col lg={5} md={6} xl={4} sm={8} xs={9}>
-            <Card >
-              <CardBody className="p-4">
-                <div>
-                  <div className="text-center">
-                    <Link to="/">
-                      <img
-                        src={logodark}
-                        alt=""
-                        height="85"
-                        className="auth-logo logo-dark mx-auto"
-                      />
-                      <img
-                        src={logolight}
-                        alt=""
-                        height="85"
-                        className="auth-logo logo-light mx-auto"
-                      />
-                    </Link>
-                  </div>
-                  <h4 className="font-size-18 text-primary mt-2 text-center">
-                    Bienvenido/a !
-                  </h4>
-                  <p className="mb-3 text-center">
-                    Inicia Sesión para acceder a Mr. Paquetes
-                  </p>
-                  <Form
-                    className="form-horizontal"
-                    onSubmit={(e) => {
-                      e.preventDefault();
-                      validation.handleSubmit();
-                      return false;
-                    }}
-                  >
-                    {error ? (
-                      <Alert color="danger">
-                        <div>{error}</div>
-                      </Alert>
-                    ) : null}
-                    <Row>
-                      <Col md={12}>
-                        <div className="mb-4">
-                          <Label className="form-label">Email</Label>
-                          <Input
-                            name="email"
-                            className="form-control"
-                            placeholder="Enter email"
-                            type="email"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            value={validation.values.email || ""}
-                            invalid={
-                              validation.touched.email &&
-                              validation.errors.email
-                                ? true
-                                : false
-                            }
-                          />
-                          {validation.touched.email &&
-                          validation.errors.email ? (
-                            <FormFeedback type="invalid">
-                              <div>{validation.errors.email}</div>
-                            </FormFeedback>
+          <div className="account-pages my-2 pt-3">
+            <Container>
+              <Row className="justify-content-center">
+                <Col lg={5} md={6} xl={4} sm={8} xs={9}>
+                  <Card>
+                    <CardBody className="p-4">
+                      <div>
+                        <div className="text-center">
+                          <Link to="/">
+                            <img
+                              src={logodark}
+                              alt=""
+                              height="85"
+                              className="auth-logo logo-dark mx-auto"
+                            />
+                            <img
+                              src={logolight}
+                              alt=""
+                              height="85"
+                              className="auth-logo logo-light mx-auto"
+                            />
+                          </Link>
+                        </div>
+                        <h4 className="font-size-18 text-primary mt-2 text-center">
+                          Bienvenido/a !
+                        </h4>
+                        <p className="mb-3 text-center">
+                          Inicia Sesión para acceder a Mr. Paquetes
+                        </p>
+                        <Form
+                          className="form-horizontal"
+                          onSubmit={(e) => {
+                            e.preventDefault();
+                            validation.handleSubmit();
+                            return false;
+                          }}
+                        >
+                          {error ? (
+                            <Alert color="danger">
+                              <div>{error}</div>
+                            </Alert>
                           ) : null}
-                        </div>
-                        <div className="mb-4">
-                          <Label className="form-label">Password</Label>
-                          <Input
-                            name="password"
-                            value={validation.values.password || ""}
-                            type="password"
-                            placeholder="Enter Password"
-                            onChange={validation.handleChange}
-                            onBlur={validation.handleBlur}
-                            invalid={
-                              validation.touched.password &&
-                              validation.errors.password
-                                ? true
-                                : false
-                            }
-                          />
-                          {validation.touched.password &&
-                          validation.errors.password ? (
-                            <FormFeedback type="invalid">
-                              <div> {validation.errors.password} </div>
-                            </FormFeedback>
-                          ) : null}
-                        </div>
+                          <Row>
+                            <Col md={12}>
+                              <div className="mb-4">
+                                <Label className="form-label">Email</Label>
+                                <Input
+                                  name="email"
+                                  className="form-control"
+                                  placeholder="Enter email"
+                                  type="email"
+                                  onChange={validation.handleChange}
+                                  onBlur={validation.handleBlur}
+                                  value={validation.values.email || ""}
+                                  invalid={
+                                    validation.touched.email &&
+                                    validation.errors.email
+                                      ? true
+                                      : false
+                                  }
+                                />
+                                {validation.touched.email &&
+                                validation.errors.email ? (
+                                  <FormFeedback type="invalid">
+                                    <div>{validation.errors.email}</div>
+                                  </FormFeedback>
+                                ) : null}
+                              </div>
+                              <div className="mb-4">
+                                <Label className="form-label">Password</Label>
+                                <Input
+                                  name="password"
+                                  value={validation.values.password || ""}
+                                  type="password"
+                                  placeholder="Enter Password"
+                                  onChange={validation.handleChange}
+                                  onBlur={validation.handleBlur}
+                                  invalid={
+                                    validation.touched.password &&
+                                    validation.errors.password
+                                      ? true
+                                      : false
+                                  }
+                                />
+                                {validation.touched.password &&
+                                validation.errors.password ? (
+                                  <FormFeedback type="invalid">
+                                    <div> {validation.errors.password} </div>
+                                  </FormFeedback>
+                                ) : null}
+                              </div>
 
-                        <Row>
-                          <Col>
-                            <div className="form-check">
-                              <input
-                                type="checkbox"
-                                className="form-check-input"
-                                id="customControlInline"
-                              />
-                              <label
-                                className="form-label form-check-label"
-                                htmlFor="customControlInline"
-                              >
-                                Recuerdame
-                              </label>
-                            </div>
-                          </Col>
-                          <Col className="col-12">
-                            <div className="text-md-end mt-3 mt-md-0">
-                              <Link to="/auth-recoverpw" className="text-muted">
-                                <i className="mdi mdi-lock"></i> Olvidaste tu
-                                contraseña?
-                              </Link>
-                            </div>
-                          </Col>
-                        </Row>
-                        <div className="d-grid mt-4">
-                          <button
-                            className="btn btn-primary waves-effect waves-light"
-                            type="submit"
-                            style={{ backgroundColor: "#635bff" }}
-                          >
-                            Iniciar Sesión
-                          </button>
-                          <p className="text-dark-50 text-right">
-                            No tienes cuenta?{" "}
-                            <Link
-                              to="/register"
-                              className="fw-medium text-primary"
-                            >
-                              {" "}
-                              Registrate{" "}
-                            </Link>{" "}
-                          </p>
-                        </div>
-                      </Col>
-                    </Row>
-                  </Form>
-                </div>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      </div></ul>
-    </div >
+                              <Row>
+                                <Col>
+                                  <div className="form-check">
+                                    <input
+                                      type="checkbox"
+                                      className="form-check-input"
+                                      id="customControlInline"
+                                    />
+                                    <label
+                                      className="form-label form-check-label"
+                                      htmlFor="customControlInline"
+                                    >
+                                      Recuerdame
+                                    </label>
+                                  </div>
+                                </Col>
+                                <Col className="col-12">
+                                  <div className="text-md-end mt-3 mt-md-0">
+                                    <Link
+                                      to="/auth-recoverpw"
+                                      className="text-muted"
+                                    >
+                                      <i className="mdi mdi-lock"></i> Olvidaste
+                                      tu contraseña?
+                                    </Link>
+                                  </div>
+                                </Col>
+                              </Row>
+                              <div className="d-grid mt-4">
+                                <button
+                                  className="btn btn-primary waves-effect waves-light"
+                                  type="submit"
+                                  style={{ backgroundColor: "#635bff" }}
+                                >
+                                  Iniciar Sesión
+                                </button>
+                                <p className="text-dark-50 text-right">
+                                  No tienes cuenta?{" "}
+                                  <Link
+                                    to="/register"
+                                    className="fw-medium text-primary"
+                                  >
+                                    {" "}
+                                    Registrate{" "}
+                                  </Link>{" "}
+                                </p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </Form>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </ul>
+      </div>
     </React.Fragment>
   );
 };
